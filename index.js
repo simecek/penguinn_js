243a8a7d824ee12f6eb7e958b84312757d81985a
Petr Simecek <lamparna@gmail.com>
2020-05-19 11:24:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+async function makePrediction() {
+  
+  var prob = document.getElementById('prob2');
+  var seq = document.getElementById('text1');
+  
+  console.log("model loading..");
+
+  // clear the model variable
+  var model = undefined;
+  
+  model = await tf.loadLayersModel("https://raw.githubusercontent.com/simecek/penguinn_js/master/assets/model.json");
+  
+  console.log("model loaded...");
+
+  const s = seq.value;  
+  const t=s.replace(/A/g,'0').replace(/T/g,'1').replace(/U/g,'1').replace(/C/g,'2').replace(/G/g,'3')
+  const y = tf.oneHot(tf.tensor1d(t.split(''),'int32'),4);
+  const z = y.reshape([1,200,4]);
+  
+  const result = model.predict(z).asScalar().dataSync();
+
+  console.log("prediction done...");
+  
+  prob.innerHTML = "<br><br>Probability of G4 complex:   " + result;
+}
+
+// Example of valid input: 
+// GAGACACCACTACAGTTAGCAGTGAGTGTAAAATAATGAGTGTCAGAAACTTATATTGGGTGATTTCATTTTTAAAAGTAACCAAAGTGAAAAATGAAGCCTTGCGTTTTTGCTTAAATGATTTACAAAAAATATTTGATGTCCATCCTGGGATAGGGAATTCCTCCCCCATAACTTTGAAAGTGCAGTTGCTTCATTCC
